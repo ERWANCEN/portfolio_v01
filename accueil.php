5d127a9aa90ef520caad913c76725c4d370d6035
ERWANCEN <erwance@hotmail.fr>
2025-01-29 01:09:30 +0100

    Ajout effet de hover sur les cta ainsi que la mise en place du dark-mode

--- accueil.php
+++ accueil.php
@@ -7,66 +7,24 @@
 
         <header>
             <div id="container_header_tablette_desktop">
-                <img class="logo_nav_mode_jour" src="assets/images/logo_noir_sans_baseline.webp" alt="logo de Erwan CÉNAC" loading="lazy">
+                <div id="logo_container">
+                    <img class="logo_nav_mode_jour logo_visible" src="assets/images/logo_noir_sans_baseline.webp" alt="logo noir" loading="lazy">
+                    <img class="logo_nav_mode_nuit" src="assets/images/logo_blanc_sans_baseline.webp" alt="logo blanc" loading="lazy">
+                </div>
+
                 <div id="container_header">
                     <?php include "inc/nav.inc.php"; ?>
                     <img src="assets/images/france.webp" alt="" class="mode_langue" loading="lazy">
-                    <div class="toggle-switch">
-                        <!-- <label class="switch-label">
-                            <input type="checkbox" class="checkbox">
-                            <span class="slider"></span>
-                        </label> -->
-                        <!-- From Uiverse.io by Creatlydev --> 
-                        <div class="container">
-                            <label class="toggle" for="switch">
-                                <input id="switch" class="input" type="checkbox">
-                                
-                                <div class="icon icon--sun">
-                                    <svg height="24" width="24" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path clip-rule="evenodd" d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" fill-rule="evenodd"></path>
-                                    </svg>
-                                </div>
-
-                                <div class="icon icon--moon">
-                                    <svg height="24" width="24" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"></path>
-                                    </svg>
-                                </div>
-                            </label>
-                        </div>
-                    </div>  
+                    <div id="mode_jour_nuit_container">
+                        <img id="mode_jour_nuit" src="assets/images/soleil_blanc.svg" alt="soleil indiquant le mode jour">
+                    </div>
                 </div>
             </div>
 
             <div id="container_header_mobile">
                 <img src="assets/images/logo_noir_sans_baseline.webp" alt="logo de Erwan CÉNAC" class="logo_nav_mode_jour" loading="lazy">
                 <img src="assets/images/france.webp" alt="" class="mode_langue" loading="lazy">
-
-                <!-- <div class="toggle-switch">
-                    <label class="switch-label">
-                        <input type="checkbox" class="checkbox">
-                        <span class="slider"></span>
-                    </label>
-                </div>  -->
-
-                <!-- From Uiverse.io by Creatlydev --> 
-                <div class="container">
-                    <label class="toggle" for="switch">
-                        <input id="switch" class="input" type="checkbox">
-                        <div class="icon icon--moon">
-                            <svg height="32" width="32" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path clip-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" fill-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                    
-                        <div class="icon icon--sun">
-                            <svg height="32" width="32" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"></path>
-                            </svg>
-                        </div>
-                    </label>
-                </div>
-
+                <img id="mode_jour_nuit" src="assets/images/soleil.svg" alt="soleil indiquant le mode jour">
                 <label class="burger" for="burger">
                     <input type="checkbox" id="burger">
                     <span></span>
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,45 @@
 'use strict';
 
-let slider = document.getElementsByClassName('slider');
-slider.addEventListener('click', () => {
-    console.log("Coucou");
-    document.getElementsByTagName('body').style.background = '#1A1A2E';
-})
+let counter = 0;
+let modeJourNuit = document.getElementById('mode_jour_nuit');
+let modeJourNuitContainer = document.getElementById('mode_jour_nuit_container');
+let body = document.querySelector('body');
+let textesDarkMode = document.querySelectorAll('.texte_dark_mode'); // Textes à changer
+let logoJour = document.querySelector('.logo_nav_mode_jour'); // Logo noir (jour)
+let logoNuit = document.querySelector('.logo_nav_mode_nuit'); // Logo blanc (nuit)
+
+modeJourNuitContainer.addEventListener('click', () => {
+    console.log(counter);
+
+    if (counter % 2 === 0) {
+        // Mode nuit
+        body.style.background = '#1A1A2E'; 
+        modeJourNuit.setAttribute('src', 'assets/images/lune_noire.svg');
+        modeJourNuitContainer.classList.add('nuit'); // Active le mode nuit
+
+        // Texte passe en mode nuit
+        textesDarkMode.forEach(element => {
+            element.classList.add('nuit');
+        });
+
+        // Changer logo (jour vers nuit)
+        logoJour.classList.remove('logo_visible');
+        logoNuit.classList.add('logo_visible');
+    } else {
+        // Mode jour
+        body.style.background = '#FFFDF2'; 
+        modeJourNuit.setAttribute('src', 'assets/images/soleil.svg');
+        modeJourNuitContainer.classList.remove('nuit'); // Désactive le mode nuit
+
+        // Texte passe en mode jour
+        textesDarkMode.forEach(element => {
+            element.classList.remove('nuit');
+        });
+
+        // Changer logo (nuit vers jour)
+        logoNuit.classList.remove('logo_visible');
+        logoJour.classList.add('logo_visible');
+    }
+
+    counter++; 
+});
